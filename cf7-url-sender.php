bbc51a5fb438f16686d1410fae19bae6a8c28b6c
Lucas Guimarães <lucas.guimaraes@grupogen.com.br>
2019-06-19 18:54:20 -0300

    Add form select on settings

--- cf7-url-sender.php
+++ cf7-url-sender.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: CF7 Data to URL Settings
+ * Plugin Name: CF7 Data Send
  * Plugin URI: https://github.com/lucasgiovanny/cf7-data-send
  * Description: Plugin that validates and send CF7 data to an URL
  * Version: 1.0
@@ -8,38 +8,49 @@
  * Author URI: http://www.lucasgiovanny.com
  */
 
-function activate_cf7urlsender() {
+function activate_cf7datasend()
+{
     add_option('base_url', 'https://exemple.com/');
-  }
-  
-  function deactive_cf7urlsender() {
+}
+
+function deactive_cf7datasend()
+{
     delete_option('base_url');
-  }
-
-function admin_init_cf7urlsender() {
-    register_setting('cf7urlsender', 'base_url');
-  }
-
-function admin_menu_cf7urlsender() {
-    add_options_page('CF7 Data to URL Settings', 'CF7 Data to URL', 'manage_options', 'cf7urlsender', 'options_page_cf7urlsender');
+    delete_option('form_id');
 }
 
-function options_page_cf7urlsender() {
-    include(WP_PLUGIN_DIR.'/cf7-url-sender/options.php');  
+function admin_init_cf7datasend()
+{
+    register_setting('cf7datasend', 'base_url');
+    register_setting('cf7datasend', 'form_id');
 }
 
-function cf7urlsender($contact_form) {
+function admin_menu_cf7datasend()
+{
+    add_options_page('CF7 Data to URL Settings', 'CF7 Data to URL', 'manage_options', 'cf7datasend', 'options_page_cf7datasend');
+}
 
+function options_page_cf7datasend()
+{
+    include WP_PLUGIN_DIR . '/cf7-data-send/options.php';
+}
+
+function cf7datasend($contact_form)
+{
+
+    $url = get_option('base_url');
+
+    // confere_se é o form
     // seta variavel com url das configs
     // seta os campos
     //  valida campos criticos
     // monta url final
     //  converte para url
     // chama curl
-    
+
     $title = $contact_form->title;
     $submission = WPCF7_Submission::get_instance();
-    if ( $submission ) {
+    if ($submission) {
         $posted_data = $submission->get_posted_data();
     }
 
@@ -55,14 +66,14 @@ function cf7urlsender($contact_form) {
     // $source = $posted_data["email"];
     // $medio = $posted_data["email"];
     // $email = $posted_data["email"];
-    
+
     // Valida os dados
     // Trata os dados
     // Monta URL (Checa a barra no final)
     // Envia a URL
 
     $url = "http://localhost:8888/freelancer/re.php?email=" . $name;
-   
+
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -77,15 +88,11 @@ function cf7urlsender($contact_form) {
 }
 
 if (is_admin()) {
-    add_action('admin_init', 'admin_init_cf7urlsender');
-    add_action('admin_menu', 'admin_menu_cf7urlsender');
+    add_action('admin_init', 'admin_init_cf7datasend');
+    add_action('admin_menu', 'admin_menu_cf7datasend');
 }
 
-register_activation_hook(__FILE__, 'activate_cf7urlsender');
-register_deactivation_hook(__FILE__, 'deactive_cf7urlsender');
+register_activation_hook(__FILE__, 'activate_cf7datasend');
+register_deactivation_hook(__FILE__, 'deactive_cf7datasend');
 
-add_action('wpcf7_before_send_mail', 'cf7urlsender'); 
-
-
-
- 
+add_action('wpcf7_before_send_mail', 'cf7datasend');
--- options.php
+++ options.php
@@ -2,14 +2,31 @@
 <h2>CF7 Data to URL Settings</h2>
 
 <form method="post" action="options.php">
-<?php wp_nonce_field('update-options'); ?>
-<?php settings_fields('cf7urlsender'); ?>
+<?php wp_nonce_field('update-options');?>
+<?php settings_fields('cf7datasend');?>
 
 <table class="form-table">
 
 <tr valign="top">
 <th scope="row">Base URL to send data:</th>
 <td><input type="text" name="base_url" value="<?php echo get_option('base_url'); ?>" /></td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Form to be watched:</th>
+<td>
+<select name="form_id" id="form_id">
+    <option value="">-- Select --</option><?php
+$dbValue = get_option('form_id'); //example!
+$posts = get_posts(array(
+    'post_type' => 'wpcf7_contact_form',
+    'numberposts' => -1,
+));
+foreach ($posts as $p) {
+    echo '<option value="' . $p->ID . '"' . selected($p->ID, $dbValue, false) . '>' . $p->post_title . ' (' . $p->ID . ')</option>';
+}?>
+</select>
+</td>
 </tr>
 
 </tr>
@@ -19,7 +36,7 @@
 <input type="hidden" name="action" value="update" />
 
 <p class="submit">
-<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+<input type="submit" class="button-primary" value="<?php _e('Save Changes')?>" />
 </p>
 
 </form>
